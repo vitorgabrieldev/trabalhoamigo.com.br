b49998720a46760c50daf4d0a96e21ae1eae6bb0
Vitor Gabriel de Oliveira <vitorgabrieldeoliveiragit@gmail.com>
2024-10-14 16:24:12 -0300

    Feat | Ajustes

--- src/views/contratante/PaginaInicial/index.php
+++ src/views/contratante/PaginaInicial/index.php
@@ -32,20 +32,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
-    <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/670d6ceb4304e3196ad17d2a/1ia672tsb';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-        })();
-    </script>
-    <!--End of Tawk.to Script-->
-
 <body>
 
     <script>
